45b584130b688efb7ad1ddf0e89c52e12483f387
Shreyas <skraman1999@gmail.com>
2015-09-12 14:27:12 -0400

    Moved decoding logic into submodule.

--- cosmicmodem.py
+++ cosmicmodem.py
@@ -1,5 +1,39 @@
+from scipy.signal import fftconvolve
+from numpy import argmax, diff
+from matplotlib.mlab import find
+
+BAUD_RATE = 4000
+
+freq = [ 350, 400, 450, 500, 550, 600, 650, 700 ]
+
 def encode(bytes):
 	return bytes
 
+def parabolic(f, x):
+    xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
+    yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
+    return (xv, yv)
+
+def freq_from_autocorr(sig, fs):
+    # Calculate autocorrelation (same thing as convolution, but with 
+    # one input reversed in time), and throw away the negative lags
+    corr = fftconvolve(sig, sig[::-1], mode='full')
+    corr = corr[len(corr)/2:]
+
+    print corr
+    
+    # Find the first low point
+    d = diff(corr)
+    start = find(d > 0)[0]
+    
+    # Find the next peak after the low point (other than 0 lag).  This bit is 
+    # not reliable for long signals, due to the desired peak occurring between 
+    # samples, and other peaks appearing higher.
+    # Should use a weighting function to de-emphasize the peaks at longer lags.
+    peak = argmax(corr[start:]) + start
+    px, py = parabolic(corr, peak)
+    
+    return fs / px
+
 def decode(bytes):
-	return bytes
+	return freq_from_autocorr(bytes, BAUD_RATE)*2
